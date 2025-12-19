c3b779f937ffe40ecdcf87e30fadb8299d9f43ee
Zaq <zaqnombre@yahoo.com>
2025-12-19 10:20:56 -0700

    Update HomeFeed: FAB + composer + toggles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,4 @@
 import ReactDOM from "react-dom/client";
-import { InternetIdentityProvider } from "./hooks/useInternetIdentity";
-import { AuthProvider } from "./hooks/useAuth";
 import { initEditor } from "./hooks/useEditor";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
@@ -8,6 +6,8 @@ import "./index.css";
 
 import SplashGate from "./components/SplashGate";
 import Logo from "./assets/afroconnect-logo.png";
+
+import { AuthProvider } from "./hooks/useAuth";
 
 const queryClient = new QueryClient();
 
@@ -20,11 +20,9 @@ if (document.readyState === "loading") {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <AuthProvider>
-      <InternetIdentityProvider>
-        <SplashGate logoSrc={Logo} slogan="Connecting Africans Globally" durationMs={5000}>
-          <App />
-        </SplashGate>
-      </InternetIdentityProvider>
+      <SplashGate logoSrc={Logo} slogan="Connecting Africans Globally" durationMs={5000}>
+        <App />
+      </SplashGate>
     </AuthProvider>
   </QueryClientProvider>
 );
